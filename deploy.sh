91e98e261a9823447848946a0ade9ab5f1025979
Chaitanya Tata <chaitanya.mgit@gmail.com>
2018-01-30 01:06:56 +0530

    basic script to install few default packages
    This is still a basic version.

---
+++ deploy.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+## for ubuntu
+#. /etc/init.d/functions
+#set -e 
+myexec() {
+    tput init
+    echo "Executing $@"
+    "$@" 2>&1 > /dev/null
+    status=$?
+    if [ $status -ne 0 ]; then
+        echo -e "\e[31merror with \e[1m$1" >&2
+	exit $status
+    fi
+    return $status
+}
+
+# Use step(), try(), and next() to perform a series of commands and print
+# [  OK  ] or [FAILED] at the end. The step as a whole fails if any individual
+# command fails.
+#
+
+## Install Necessary packages
+myexec apt-get install -y vim
+myexec sudo apt-get install -y vim-gtk
+myexec sudo apt-get install -y exuberant-ctags
+myexec sudo apt-get install -y ack-grep
+myexec yes | sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+myexec sudo apt-get update
+myexec eudo apt-get install grub-customizer
+
+
+##setup init files
+## configure vimrc
+## configure alias-es in bashrc
